34988e835c9a4c035028668a2afcc67224027490
lgh06 <hnnk@qq.com>
2017-04-21 23:35:24 +0800

    init

---
+++ front/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "kankan",
+  "parserOptions": {
+    "sourceType": "module"
+  }
+};
